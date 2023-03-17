969c5fdc5286dd63d96480ab1662eae2d7e4b33e
Thant Ko Zaw <thantmkou10@gmail.com>
2023-03-17 21:58:25 +0630

    Schemas fixed

--- src/schema/registerUserBody.schema.js
+++ src/schema/registerUserBody.schema.js
@@ -22,14 +22,16 @@ const registerUserBodySchema = z.object({
   ),
   jobLabel: z.string({ required_error: 'Job label is required.' }),
   permissions: z.object({
-    requestForm: z.object({
-      read: z.boolean().default(true),
+    adminSection: z.object({
       approve: z.boolean().default(false),
       reject: z.boolean().default(false),
       verify: z.boolean().default(false),
-      submit: z.boolean().default(false),
-      update: z.boolean().default(false),
-      delete: z.boolean().default(false),
+    }),
+    fadSection: z.object({
+      approve: z.boolean().default(false),
+      reject: z.boolean().default(false),
+      verify: z.boolean().default(false),
+      acknowledge: z.boolean().default(false),
     }),
   }),
   approvalAmount: z
