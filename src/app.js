748ac4e18d5abf86d1e69514b23e2b4cd39c44fe
Thant Ko Zaw <thantmkou10@gmail.com>
2023-03-28 09:57:45 +0630

    Allow cors from anywhere

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const userRouter = require('./routes/user.route');
 
 const app = express();
 
-app.use(cors());
+app.use(cors('*'));
 app.use(helmet());
 app.use(express.json());
 
--- src/lib/s3.js
+++ src/lib/s3.js
@@ -24,6 +24,7 @@ function uploadFile(file) {
       Bucket: AWS_S3_BUCKET_NAME,
       Key: key,
       Body: file.buffer,
+      ACL: 'public-read',
     })
     .promise();
 }
