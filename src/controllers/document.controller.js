deedcff1366ada25759a5fe4781952fc9938cb00
Thant Ko Zaw <thantmkou10@gmail.com>
2023-03-28 10:21:03 +0630

    Fix/s3

--- src/controllers/document.controller.js
+++ src/controllers/document.controller.js
@@ -9,6 +9,7 @@ const documentService = require('../services/document.service');
 
 const createDocumentController = () => {
   const createDocument = catchAsync(async (req, res, next) => {
+    console.log(req.files)
     const attachments = await documentService.uploadAttachments(req.files);
     const document = await documentService.createRequisitionDocument({
       ...req.body,
