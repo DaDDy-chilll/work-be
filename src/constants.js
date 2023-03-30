01479d95e4e83e1bbaece3dc659e32ecce3f7ad8
Thant Ko Zaw <thantmkou10@gmail.com>
2023-03-30 14:25:52 +0630

    Change NODE_ENV

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'local',
   MONGODB_URI: process.env.MONGODB_URI,
   DB_NAME: process.env.DATABASE_NAME,
   JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET,
