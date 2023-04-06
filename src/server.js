5e9e0eec85658688b9135ae04845e66e1d315ae9
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-06 13:35:59 +0630

    Clean up server.js

--- src/server.js
+++ src/server.js
@@ -4,27 +4,37 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const { PORT, MONGODB_URI, DB_NAME } = require('./constants');
 
-mongoose.set('strictQuery', false);
+async function connectToDatabase() {
+  try {
+    mongoose.set('strictQuery', false);
 
-mongoose
-  .connect(MONGODB_URI, {
-    dbName: DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => {
-    console.error('MongoDB Error: ', error);
+    await mongoose.connect(MONGODB_URI, {
+      dbName: DB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected to MongoDB.');
+  } catch (error) {
+    console.log('Error while connecting to MongoDB');
+    console.error(error);
+  }
+}
+
+async function main() {
+  await connectToDatabase();
+
+  const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log('UnhandledRejection occurred.');
-  console.log(err);
-  server.close(() => {
-    process.exit(1);
+  process.on('unhandledRejection', (err) => {
+    console.log('UnhandledRejection occurred.');
+    console.log(err);
+    server.close(() => {
+      process.exit(1);
+    });
   });
-});
+}
+
+main();
