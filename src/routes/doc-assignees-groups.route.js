ebf8174a2a862cd4329ee8a402fc5de0cccdd1ff
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-18 15:43:37 +0630

    Add auth middleware

--- src/routes/doc-assignees-groups.route.js
+++ src/routes/doc-assignees-groups.route.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
 
+const { userRoles } = require('../constants');
 const docAssigneesGroupsController = require('../controllers/doc-assignees-groups.controller');
+const authenticate = require('../middlewares/authenticate');
+const authorize = require('../middlewares/authorize');
 const validate = require('../middlewares/validate');
 const createAssigneeSchema = require('../schema/createAssigneesGroup');
 
 router.post(
   '/',
+  authenticate,
+  authorize([userRoles.superadmin]),
   validate(createAssigneeSchema),
   docAssigneesGroupsController.createAssigneeGroup
 );
 
-router.get('/', docAssigneesGroupsController.getAssigneesGroup);
+router.get('/', authenticate, docAssigneesGroupsController.getAssigneesGroup);
 
 module.exports = router;
