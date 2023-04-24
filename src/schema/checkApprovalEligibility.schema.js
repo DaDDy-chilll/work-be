cf4ef9b3b9a253304bda9745195c6d0a0b06abe5
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-24 16:28:27 +0630

    Parse the array sent via form-data

--- src/schema/checkApprovalEligibility.schema.js
+++ src/schema/checkApprovalEligibility.schema.js
@@ -2,9 +2,9 @@ const { isObjectIdOrHexString } = require('mongoose');
 const { z } = require('zod');
 
 const checkApprovalEligibility = z.object({
-  body: z.object({
-    reviewers: z.string().refine(isObjectIdOrHexString, 'Invalid ID').array(),
-  }),
+  // body: z.object({
+  //   reviewers: z.string().refine(isObjectIdOrHexString, 'Invalid ID').array(),
+  // }),
   params: {
     dept: z.enum(['admin', 'fad']),
   },
