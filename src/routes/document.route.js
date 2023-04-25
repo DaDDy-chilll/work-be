189272f3002dd8fc776ac2197b8c7bc840b8467e
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-25 09:25:10 +0630

    Add quick fix to parse document payload

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -27,6 +27,16 @@ router.post(
   '/',
   authenticate,
   upload.array('attachments'),
+  function (req, res, next) {
+    if (typeof req.body.adminReviewers === 'string') {
+      req.body.adminReviewers = JSON.parse(req.body.adminReviewers);
+    }
+    if (typeof req.body.fadReviewers === 'string') {
+      req.body.fadReviewers = JSON.parse(req.body.fadReviewers);
+    }
+
+    next();
+  },
   validate(createDocumentSchema),
   documentController.createDocument
 );
