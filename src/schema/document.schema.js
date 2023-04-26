fe5d8b6a262904281f3f0c5ccfb81b6eeaab2545
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-26 10:58:36 +0630

    Add schema for getting documents

--- src/schema/document.schema.js
+++ src/schema/document.schema.js
@@ -1,6 +1,11 @@
 const { z } = require('zod');
+const {
+  DOCUMENT_STATUSES,
+  DOCUMENT_SECTIONS,
+} = require('../constants/document');
+const { isObjectIdOrHexString } = require('mongoose');
 
-const getDocument = z.object({
+const getDocuments = z.object({
   query: z
     .object({
       sort: z.string().default('-createdAt'),
@@ -9,7 +14,20 @@ const getDocument = z.object({
         .int('`limit` must be positive integer.')
         .positive('`limit` must be positive integer.')
         .default(10),
-      status: z.string(),
+      status: z
+        .enum(Object.values(DOCUMENT_STATUSES))
+        .or(z.array(z.enum(Object.values(DOCUMENT_STATUSES)))),
+      section: z.enum(Object.values(DOCUMENT_SECTIONS)),
+      amount: z.coerce.number().nonnegative(),
+      amountMin: z.coerce.number().nonnegative(),
+      amountMax: z.coerce.number().nonnegative(),
+      requestedBy: z.string().refine(isObjectIdOrHexString),
+      currentReviewer: z.string().refine(isObjectIdOrHexString),
     })
-    .partial(),
+    .partial()
+    .strict(),
 });
+
+module.exports = {
+  getDocuments,
+};
