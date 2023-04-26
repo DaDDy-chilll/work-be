152efda810c21ceaf76a0d4078ef0802f28b9864
Thant Ko Zaw <thantmkou10@gmail.com>
2023-04-26 13:27:29 +0630

    Add middleware in fad submission

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -108,6 +108,16 @@ router.post(
   '/fad/:id',
   validate(submitFadDocumentSchema),
   authenticate,
+  function (req, res, next) {
+    if (typeof req.body.adminReviewers === 'string') {
+      req.body.adminReviewers = JSON.parse(req.body.adminReviewers);
+    }
+    if (typeof req.body.fadReviewers === 'string') {
+      req.body.fadReviewers = JSON.parse(req.body.fadReviewers);
+    }
+
+    next();
+  },
   documentController.submitDocumentToFAD
 );
 
