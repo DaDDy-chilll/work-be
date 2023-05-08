c79d2c3a6e3896c54f4891ffd2da75ddbc3e3f76
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-08 10:08:22 +0630

    reimplement group create

--- src/schema/reviewer-group.schema.js
+++ src/schema/reviewer-group.schema.js
@@ -10,113 +10,27 @@ const {
 const BASE_GROUP = z.object({
   body: z.object({
     name: z.string({ required_error: 'Group name is required' }),
-    reviewers: z
-      .array(
-        z.object({
-          index: z.coerce
-            .number({ invalid_type_error: 'Order must start from zero.' })
-            .int()
-            .nonnegative('Order must not be negative.'),
-          reviewer: z
-            .string({ required_error: 'Reviewer ID is required.' })
-            .refine(isObjectIdOrHexString),
-          department: z.enum(
-            Object.values(AUTHORIZED_DEPARTMENTS).filter(
-              (d) => d !== 'OFFICE_ADMIN'
-            ),
-            {
-              errorMap: (_issue, _ctx) => ({
-                message: 'Invalid department.',
-              }),
-            }
-          ),
-          canEdit: z
-            .boolean({ invalid_type_error: '`canEdit` is invalid' })
-            .optional()
-            .default(true),
-          canPrepare: z
-            .boolean({ invalid_type_error: '`canPrepare` is invalid' })
-            .optional()
-            .default(true),
-          canApprove: z
-            .boolean({ invalid_type_error: '`canApprove` is invalid' })
-            .optional()
-            .default(false),
-          canVerify: z
-            .boolean({ invalid_type_error: '`canVerify` is invalid' })
-            .optional()
-            .default(true),
-        })
-      )
-      // Validate uniqueness
-      .superRefine((reviewers, ctx) => {
-        const sortedReviewersByIdx = _.sortBy(reviewers, 'index');
-
-        let departments = [];
-
-        for (let i = 0; i < sortedReviewersByIdx.length; i++) {
-          const curr = sortedReviewersByIdx[i];
-          const next = sortedReviewersByIdx[i + 1];
-
-          departments.push(curr.department);
-
-          // index should start from zero
-          // and be incrementally order
-          // Checks by comparing with iterator
-          if (i !== curr.index) {
-            ctx.addIssue({
-              code: ZodIssueCode.custom,
-              message: 'Indexes must be incrementally ordered.',
-            });
-          }
-
-          if (!next) {
-            break;
-          }
-
-          if (curr.department !== next.department) {
-            const diff =
-              DEPARTMENT_LEVELS[next.department] -
-              DEPARTMENT_LEVELS[curr.department];
-
-            // Checks correct dept order
-            if (diff !== 1) {
-              ctx.addIssue({
-                code: ZodIssueCode.custom,
-                message: `Wrong department order: ${curr.department} & ${next.department}`,
-              });
-            }
-
-            if (!curr.canApprove) {
-              ctx.addIssue({
-                code: ZodIssueCode.custom,
-                message: `Last person in ${curr.department} must have approve privilege.`,
-              });
-            }
-          } else {
-            if (curr.canApprove) {
-              ctx.addIssue({
-                code: ZodIssueCode.custom,
-                message: `Only last person in ${curr.department} can have approve privilege.`,
-              });
-            }
-          }
-        }
-
-        departments = [...new Set([...departments])];
-
-        if (
-          departments.length !==
+    reviewers: z.array(
+      z.object({
+        index: z.coerce
+          .number({ invalid_type_error: 'Order must start from zero.' })
+          .int()
+          .nonnegative('Order must not be negative.'),
+        reviewer: z
+          .string({ required_error: 'Reviewer ID is required.' })
+          .refine(isObjectIdOrHexString),
+        department: z.enum(
           Object.values(AUTHORIZED_DEPARTMENTS).filter(
             (d) => d !== 'OFFICE_ADMIN'
-          ).length
-        ) {
-          ctx.addIssue({
-            code: ZodIssueCode.custom,
-            message: 'Missing department(s)',
-          });
-        }
-      }),
+          ),
+          {
+            errorMap: (_issue, _ctx) => ({
+              message: 'Invalid department.',
+            }),
+          }
+        ),
+      })
+    ),
   }),
 });
 
--- src/services/reviewer-groups.service.js
+++ src/services/reviewer-groups.service.js
@@ -1,9 +1,88 @@
+const _ = require('lodash');
+
+const {
+  DEPARTMENT_LEVELS,
+  AUTHORIZED_DEPARTMENTS,
+} = require('../constants/user');
 const ApiError = require('../helpers/apiError');
 
 module.exports = ({ ReviewerGroup, userService }) => {
-  const validateReviewerGroup = async (group) => {
-    const userIds = group.reviewers.map(({ reviewer }) => reviewer);
-    const users = await Promise.all(userService.getUserById);
+  const validateReviewerGroup = async (reviewers) => {
+    let tempReviewers = await Promise.all(
+      reviewers.map(async (item) => {
+        const reviewer = await userService.getUserById(item.reviewer);
+        return {
+          ...item,
+          reviewer,
+        };
+      })
+    );
+
+    const sortedReviewersByIdx = _.sortBy(tempReviewers, 'index');
+
+    let departments = [];
+
+    for (let i = 0; i < sortedReviewersByIdx.length; i++) {
+      const curr = sortedReviewersByIdx[i];
+      const next = sortedReviewersByIdx[i + 1];
+
+      departments.push(curr.department);
+
+      // index should start from zero
+      // and be incrementally order
+      // Checks by comparing with iterator
+      if (i !== curr.index) {
+        return {
+          status: false,
+          message: 'Indexes must be incrementally ordered.',
+        };
+      }
+
+      if (!next) {
+        break;
+      }
+
+      if (curr.department !== next.department) {
+        const diff =
+          DEPARTMENT_LEVELS[next.department] -
+          DEPARTMENT_LEVELS[curr.department];
+
+        // Checks correct dept order
+        if (diff !== 1) {
+          return {
+            status: false,
+            message: `Wrong department order: ${curr.department} & ${next.department}`,
+          };
+        }
+
+        if (!curr.reviewer.permissions.canApprove) {
+          return {
+            status: false,
+            message: `Last person in ${curr.department} must have approve privilege.`,
+          };
+        }
+      } else {
+        if (curr.reviewer.permissions.canApprove) {
+          return {
+            status: false,
+            message: `Only last person in ${curr.department} can have approve privilege.`,
+          };
+        }
+      }
+    }
+
+    if (
+      departments.length !==
+      Object.values(AUTHORIZED_DEPARTMENTS).filter((d) => d !== 'OFFICE_ADMIN')
+        .length
+    ) {
+      return {
+        status: false,
+        message: 'Missing department(s)',
+      };
+    }
+
+    return { status: true };
   };
   const getReviewersGroup = async () => {
     const groups = await ReviewerGroup.find().populate('reviewers.reviewer');
@@ -12,6 +91,12 @@ module.exports = ({ ReviewerGroup, userService }) => {
   };
 
   const createReviewerGroup = async (data) => {
+    const validation = await validateReviewerGroup(data.reviewers);
+
+    if (!validation.status) {
+      throw ApiError.badRequest(validation.message);
+    }
+
     return await ReviewerGroup.create(data);
   };
 
