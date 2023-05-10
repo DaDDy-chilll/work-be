6dceaa871d3bca11e5bc24ee17d431df58525c3e
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-10 10:43:24 +0630

    Add timestamps

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -5,29 +5,34 @@ const Schema = mongoose.Schema;
 
 // noti to people who have to acknowledge
 // ['ACKNOWLEDGE']
-const notificationSchema = new Schema({
-  to: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
+const notificationSchema = new Schema(
+  {
+    to: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    from: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    action: {
+      type: String,
+      enum: Object.values(DOCUMENT_ACTIONS),
+      required: true,
+    },
+    documentId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Document',
+    },
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
   },
-  from: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  action: {
-    type: String,
-    enum: Object.values(DOCUMENT_ACTIONS),
-    required: true,
-  },
-  documentId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Document',
-  },
-  isOpen: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Notification = mongoose.model('Notification', notificationSchema);
 
