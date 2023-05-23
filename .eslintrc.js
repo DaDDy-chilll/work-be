3c1b965e1f361ad26bc12d50dec965062ee1eb00
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-23 20:10:33 +0630

    Typescript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   plugins: ['jsdoc', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -19,5 +24,6 @@ module.exports = {
     ],
     'no-duplicate-imports': 'error',
     'no-console': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
