7b04195501cf49e80fca781450c1ae4a35d998d6
Thant Ko Zaw <thantmkou10@gmail.com>
2023-05-23 20:10:33 +0630

    Typescript configuration

--- src/models/department.model.ts
+++ src/models/department.model.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import * as mongoose from 'mongoose';
 
 const departmentSchema = new mongoose.Schema({
   name: {
@@ -14,4 +14,4 @@ const departmentSchema = new mongoose.Schema({
 
 const Department = mongoose.model('Department', departmentSchema);
 
-module.exports = Department;
+export default Department;
