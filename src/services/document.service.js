5432cc11b41bfabc51ef0b715b3f60e121e13ee6
Thant Ko Zaw <thantmkou10@gmail.com>
2023-07-03 10:59:52 +0630

    Fix typo

--- src/services/document.service.js
+++ src/services/document.service.js
@@ -103,7 +103,7 @@ module.exports = ({
   };
 
   const prepareUpdater = async ({ body, files, oldAttachments, reviewer }) => {
-    if ((body.type || body.amount) && !reviewer.permissions.editAmount) {
+    if ((body.type || body.amount) && !reviewer.permissions.canEditAmount) {
       throw ApiError.badRequest('You do not have permissions to edit amount.');
     }
 
