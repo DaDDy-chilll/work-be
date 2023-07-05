108748abb2920061bdb1c796418c11e8680327a6
Thant Ko Zaw <thantmkou10@gmail.com>
2023-07-05 09:42:10 +0630

    Make amount and type optional in document action

--- src/schema/document.schema.js
+++ src/schema/document.schema.js
@@ -99,12 +99,14 @@ const DOCUMENT_ACTION = z.object({
         .string()
         .min(2, 'Name must have at least 2 characters.')
         .max(50, 'Name must have at most 50 characters.'),
-      amount: z.coerce.number().positive('Invalid amount'),
-      type: z.enum(Object.values(DOCUMENT_TYPES), {
-        errorMap: (_issue, _ctx) => {
-          return { message: 'Invalid document type.' };
-        },
-      }),
+      amount: z.coerce.number().positive('Invalid amount').optional(),
+      type: z
+        .enum(Object.values(DOCUMENT_TYPES), {
+          errorMap: (_issue, _ctx) => {
+            return { message: 'Invalid document type.' };
+          },
+        })
+        .optional(),
       description: z.string().transform(xss).optional(),
       remark: z.string().transform(xss),
     })
