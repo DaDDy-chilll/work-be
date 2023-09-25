5bdcb7c81d28aafc8df97c29e88047325f8f9d79
ThaeNandarSeint <dardar2003nandar@gmail.com>
2023-09-25 10:45:35 +0630

    sort, filter, skip, limit in workflow getAll route

--- src/controllers/reviewer-groups.controller.js
+++ src/controllers/reviewer-groups.controller.js
@@ -3,7 +3,9 @@ const sendSuccessResponse = require('../utils/sendSuccessResponse');
 
 module.exports = ({ reviewerGroupService }) => {
   const getReviewersGroup = catchAsync(async (req, res, next) => {
-    const { groups, total } = await reviewerGroupService.getReviewersGroup();
+    const { groups, total } = await reviewerGroupService.getReviewersGroup(
+      req.query
+    );
 
     sendSuccessResponse({
       res,
