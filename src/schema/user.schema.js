744d15ccf7d21b8d18191d038ab11566cbc4b4b9
ThaeNandarSeint <dardar2003nandar@gmail.com>
2023-11-03 10:40:16 +0630

    change password policy

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -60,8 +60,14 @@ const UPDATE_PASSWORD = z.object({
     .object({
       password: z
         .string()
-        .min(8, 'Password must have at least 8 characters.')
-        .max(16, 'Password exceeds a maximum of 16 characters.'),
+        .min(8, 'Password must be at least 8 characters.')
+        .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
+        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+        .regex(/\d/, 'Password must contain at least one number.')
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+          message: 'Password must contain at least one special character.',
+        })
+        .max(16, 'Password must be at most 16 characters.'),
       confirmPassword: z.string().min(1, 'Please confirm password'),
     })
     .refine(
